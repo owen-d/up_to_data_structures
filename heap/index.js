85aabd90794dcc60b830d5e7e549b234e768c916
Owen Diehl <ow.diehl@gmail.com>
2016-08-15 11:44:46 -0400

    begins adding heap methods

--- heap/index.js
+++ heap/index.js
@@ -1,11 +1,109 @@
+'use strict';
+const _ = require('lodash');
 // es5
 // function Heap(order=1) {}
 // Heap.prototype.constructor = Heap;
-// es6
+
+// will run min-heap first
 class Heap {
-  constructor() {
-    this._store = [];
+  constructor(size, order=-1) {
+    this.clearStore(size)
+    this._order = order;
+    this._shiftDownFilter = order < 0 ? (a, b) => b <= a : a <= b;
+    this._shiftUpFilter = _.negate(this._shiftDownFilter)
+  }
+  [Symbol.iterator] () {
+    const unallowed = [undefined, null];
+    return {
+      next: () => {
+        let cur = this.extract();
+        let res = {
+          value: cur,
+          done: _.indexOf(unallowed, cur) !== -1
+        };
+        return res;
+      }
+    };
+  }
+  insert(val) {
+    let i = this.firstOpen();
+    this._store[i] = val
+    return this.shiftUp(i);
+  }
+  extract() {
+    let val = this._store[0];
+    this._store[0] = null;
+    this.shiftDown(0)
+    return val;
+  }
+  replace() {}
+  heapify(arr) {
+    this.clearStore()
+    for (let item of arr) {
+      this.insert(item);
+    }
+  }
+  size() {
+    return this._store.length;
+  }
+  isEmpty() {
+    return this._store[0] === null;
+  }
+
+  // internal
+  compare(p, children, filter, sort) {
+    if (!sort) {throw new Error('must provide a sort order to compare')}
+    return _(children).filter(filter).orderBy(null, sort).value();
+  }
+  fetch(i) {
+    return this._store[i];
+  }
+  delete() {}
+  shiftUp(i) {
+    let val = this._store[i];
+    let [pIdx, pVal] = this.getParent(i)
+    if (this._order < 0 ? val < pVal : val > pVal) {
+      this.swap([i, val], [pIdx, pVal])
+      return this.shiftUp(pIdx);
+    } else {
+      return i;
+    }
+  }
+  shiftDown(i) {
+    let val = this._store[i];
+    // [[c1Idx, c1Val], [c2Idx, c2Val]] = this.getChildren(i);
+    let order = this._order < 0 ? 'asc': 'desc';
+    let filter = val === null ? ([i, val]) => !_.isNull(val) && !_.isUndefined(val) : this._shiftDownFilter.bind(null, val)
+    let matches = this.compare(val, this.getChildren(i), filter, order);
+    if (matches.length) {
+      this.swap([i, val], matches[0]);
+      return this.shiftDown(matches[0][0]);
+    } else {
+      val === null ? this._store[i] = undefined : void 0;
+      return i;
+    }
+  }
+  swap(a, b) {
+    // nodes take structure [idx, val]
+    this._store[a[0]] = b[1];
+    this._store[b[0]] = a[1];
+  }
+  getChildren(i) {
+    let firstIdx = 2 * i + 1;
+    let secondIdx = 2 * i + 2;
+    return [[firstIdx, this.fetch(firstIdx)], [secondIdx, this.fetch(secondIdx)]];
+  }
+  getParent(i) {
+    let idx = Math.floor((i-1)/2)
+    return [idx, this.fetch(idx)];
+  }
+  firstOpen() {
+    return _.indexOf(this._store, null);
+  }
+  clearStore(size) {
+    size = size || this._store.length
+    this._store = _.fill(Array(size), null);
   }
 }
 
-export default Heap
+module.exports = Heap;
---
+++ heap/spec.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const expect = require('chai').expect;
+const Heap = require('./');
+const _ = require('lodash');
+
+describe('Heap', () => {
+  it('should allow iteration', done => {
+    // min heap
+    let h = new Heap(5, -1);
+    h.insert(1)
+    h.insert(3)
+    h.insert(4)
+    h.insert(2)
+
+    assertOrder(h);
+    done();
+  })
+
+  it('should support heapify', done => {
+    // min heap
+    let input = initializeRandomArray(10);
+    let h = new Heap(input.length, -1);
+    h.heapify(input)
+    console.log(input, h._store)
+    assertOrder(h);
+    done();
+  })
+});
+
+
+
+function assertOrder(heap, order=-1) {
+  let prop = order < 0 ? 'most' : 'least' ;
+  let last;
+  for (let v of heap) {
+    last = last === undefined ? v : last;
+    expect(last).to.be.at[prop](v)
+    last = v;
+  }
+}
+
+function initializeRandomArray(size) {
+  return _.map(Array(size), x => Math.floor(Math.random()* 10))
+}
